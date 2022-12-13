7995a47e0360c8bf6c61069c42c786f351b38de4
YashKsharma312 <112351428+YashKsharma312@users.noreply.github.com>
2022-12-13 15:20:56 +0530

    Update test.js

--- Test/test.js
+++ Test/test.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
-describe("Autofarm test contract", function () {
+describe("Uniswap test contract", function () {
   var owner, addr1, addr2, impersonatedSigner;
         
   var Autofarm, hardhatAutofarm, autoFarmAddress, autoFarmContract;
@@ -10,42 +10,67 @@ describe("Autofarm test contract", function () {
   it("1) Impersonating as AutoFarm owner and set allocPoint of a pool", async function () {
 
     [owner, addr1, addr2] = await ethers.getSigners();
-    Autofarm = await ethers.getContractFactory("StratX2");
+    Autofarm = await ethers.getContractFactory("Farm");
     hardhatAutofarm = await Autofarm.deploy();
-    autoFarmAddress = "0xe2e7Cf05E14e6fBBCbaA2Cd361DD99c03DFF9f72";
+
+    //var autoFarmAddress = "0x0895196562C7868C5Be92459FaE7f877ED450452";
+
+    autoFarmAddress = "0x0895196562C7868C5Be92459FaE7f877ED450452";
     autoFarmContract = await hre.ethers.getContractAt("AutoFarmV2", autoFarmAddress);
+
+
+    
     var autoOwner = await hardhatAutofarm.getOwner();
+
     await helpers.impersonateAccount(autoOwner);
     impersonatedSigner = await ethers.getSigner(autoOwner);
+
+    //console.log(impersonatedSigner);
+
     var allocValue = await autoFarmContract.poolInfo(56);
+
+    //console.log(allocValue);
     var pid = 56;
     var allocpoint = 1000;
     var booleanVal = false;
     var tx = await autoFarmContract.connect(impersonatedSigner).set(pid, allocpoint, booleanVal);
     const reciept2 = await tx.wait();
+
     poolInfo = await autoFarmContract.poolInfo(56);
+
+    //console.log(poolInfo.allocPoint);
+
+
     expect(poolInfo.allocPoint).to.equal(allocpoint);
   });
 
   it("2) Transfering ownership to addr1.", async function () {
+
     var tx = await autoFarmContract.connect(impersonatedSigner).transferOwnership(addr1.address);
     const reciept2 = await tx.wait();
+
+    //console.log(poolInfo.allocPoint);
+
+
     expect(await hardhatAutofarm.getOwner()).to.equal(addr1.address);
   });
 
   it("3) Checking total alloc points.", async function () {
 
-    var totalAlloc = await autoFarmContract.totalAllocPoint(); 
+    var totalAlloc = await autoFarmContract.totalAllocPoint();
+
+    //console.log(totalAlloc);
     var pid = 56;
-    var allocpoint = 0; 
+    var allocpoint = 0; // setting allocPoint of pool-56 to zero from 1000
     var booleanVal = false;
     var tx = await autoFarmContract.connect(impersonatedSigner).set(pid, allocpoint, booleanVal);
     const reciept2 = await tx.wait();
+
     expect(await autoFarmContract.totalAllocPoint()).to.equal((totalAlloc - 1000));
   });
 
   it("4) Check for owner",async function(){
-    realOwner="0xCF2495d59E81405b1e9320a66bcc190941D7184F"
+    realOwner="0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
     var autoOwner = await hardhatAutofarm.getOwner();
     expect(autoOwner).to.equal(realOwner);
   })
@@ -56,6 +81,5 @@ describe("Autofarm test contract", function () {
     var tx = await autoFarmContract.connect(impersonatedSigner).AUTO();
     expect(await autoFarmContract.AUTO()).to.equal("0x4508ABB72232271e452258530D4Ed799C685eccb");
   });
-
 
 });
