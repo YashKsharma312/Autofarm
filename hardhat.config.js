99ef6c20830c38f6ab0069ab6500b3fd5571f26d
YashKsharma312 <112351428+YashKsharma312@users.noreply.github.com>
2022-12-13 15:19:12 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,56 @@
+// require("@nomiclabs/hardhat-waffle");
+//require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 
-/**
- * @type import(‘hardhat/config’).HardhatUserConfig
- */
-require("dotenv").config();
-require("@nomiclabs/hardhat-ethers");
-const { API_URL, PRIVATE_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-   solidity: "0.6.12",  
-   defaultNetwork: "bsc", 
-   networks: {    
-     hardhat: {},   
-     bsc: {     
-      url: API_URL,      
-      accounts: [`0x${PRIVATE_KEY}`],   
-     }
-   }
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        },
+      },
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        },
+      },
+      
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        },
+      },
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        },
+      },
+    ],
+  },
 };
